ae5f84e72288816a80d430dacec3f45ee2fda35a
brthiagon <thiagotgn6@gmail.com>
2026-09-10 20:24:40 -0300

    feat/ id and submit button

--- checkpoint-4/app/components/notation.tsx
+++ checkpoint-4/app/components/notation.tsx
@@ -1,8 +1,14 @@
 function Notation() {
     return (
         <div className="flex min-h-screen flex-col items-center justify-between p-24">
-            <input id="title" type="text" placeholder="Title" />
-            <input id="content" type="text" placeholder="Content" />
+            <div className="input-group">
+                <input id="title" type="text" placeholder="Title" />
+                <input id="content" type="text" placeholder="Content" />
+            </div>
+        <div>
+
+        </div>
+            <button id="submit" className="btn btn-primary">submit</button>
         </div>
     )
 }
