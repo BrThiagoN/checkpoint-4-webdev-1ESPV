0f768be89f2d8c2461f186ada6484b704b3acfb6
brthiagon <thiagotgn6@gmail.com>
2026-09-10 20:15:27 -0300

    saudades da minha ex

---
+++ checkpoint-4/app/components/notation.tsx
@@ -0,0 +1,9 @@
+function Notation() {
+    return (
+        <div className="flex min-h-screen flex-col items-center justify-between p-24">
+            <input type="text" placeholder="Enter notation" />
+        </div>
+    )
+}
+
+export default Notation;
--- checkpoint-4/app/page.tsx
+++ checkpoint-4/app/page.tsx
@@ -1,10 +1,16 @@
 import { Playwrite_CA } from "next/font/google";
 import Image from "next/image";
 
+import Notation from "./components/notation";
+
 export default function Home() {
   return (
-    <header className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-3xl font-bold">HARD NOTATION</h1>
-    </header>
+    <div>
+      <header className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1 className="text-3xl font-bold">HARD NOTATION</h1>
+      </header>
+      <Notation />
+    </div>
+
   );
 }
