2f17c8ab8b00ead735e4e10bb336b1c1f65e31ae
brthiagon <thiagotgn6@gmail.com>
2026-09-10 19:58:41 -0300

    chore titulo

--- checkpoint-4/app/page.tsx
+++ checkpoint-4/app/page.tsx
@@ -1,7 +1,10 @@
+import { Playwrite_CA } from "next/font/google";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    
+    <header className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-3xl font-bold">HARD NOTATION</h1>
+    </header>
   );
 }
